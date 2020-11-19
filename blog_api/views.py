2b6607f2e7f6863dc062567653293fab759613d4
Aman singh <singh.aman0777@gmail.com.com>
2020-11-19 13:42:38 +0530

    DRF

---
+++ blog_api/views.py
@@ -0,0 +1,15 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset=Post.postobjects.all()
+    serializer_class=PostSerializer
+    pass
+class PostDetail(generics.RetrieveAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    pass
